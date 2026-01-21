ec585e1b2c77bdedd4ce866d9827805294a7ad09
relisha1 <e.rurangwa@alustudent.com>
2026-01-21 09:51:10 +0000

    add

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+/*
+ * file: 101-natural.c
+ */
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
